7581fd5e7d6dc3cd67938f830ec6803ae2f22c00
Osvaldo Trejo <OSVALDO.TREJO1@ibm.com>
2021-02-07 18:54:13 -0600

    no consoles

--- utils/auth/strategies/basic.js
+++ utils/auth/strategies/basic.js
@@ -11,7 +11,6 @@ passport.use(new BasicStrategy(async function(email, password, cb) {
     const usersService = new UsersService()
     try {
         const user = await usersService.getUser({ email })
-        console.log('hello world',user)
 
         if(!user){
             return cb(boom.unauthorized(), false)
